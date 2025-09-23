fbb4931c093b7e2c06309c8de48d52eaaef1af29
danis <ahrnadeev@gmail.com>
2025-09-23 07:45:41 +0500

    feat: add game logic functions (switchNextToMove() in SquaresGame class);

--- src/main/java/ru/ivk/model/SquaresGame.java
+++ src/main/java/ru/ivk/model/SquaresGame.java
@@ -1,16 +1,24 @@
 package ru.ivk.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter // лучше бы, наверное, private
 public class SquaresGame {
     private final Board board;
     private final User user1;
     private final User user2;
+    private User nextToMove;
 
     public SquaresGame(Board board, User user1, User user2) {
         this.board = board;
         this.user1 = user1;
         this.user2 = user2;
+        this.nextToMove = user1; // по умолчанию первый записанный игрок ходит первым
+    }
+
+    public void switchNextToMove() {
+        setNextToMove(nextToMove.equals(user1) ? user2 : user1);
     }
 }
