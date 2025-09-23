be3acef79283d0061671a6aae9257541da74dc29
danis <ahrnadeev@gmail.com>
2025-09-23 07:52:36 +0500

    refactor: simplified User's makeMove() method since game logic is in SquaresGame and Board classes;

--- src/main/java/ru/ivk/model/User.java
+++ src/main/java/ru/ivk/model/User.java
@@ -1,30 +1,25 @@
 package ru.ivk.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import ru.ivk.enums.UserColor;
 import ru.ivk.enums.UserType;
+import ru.ivk.utils.GameManager;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class User {
     private final UserType userType;
     private final UserColor userColor;
 
-    private void move(Board board, Coordinates coordinates) throws Exception {
-        int x = coordinates.getX();
-        int y = coordinates.getY();
-        if (x >= board.getSize() || y >= board.getSize()) {
-            throw new Exception("Переданы невалидные координаты!");
-        }
-        board.getBoard()[x][y] = this.userColor.getColor();
-    }
-
-    public void makeMove(Board board, Coordinates coordinates) throws Exception {
+    public void makeMove(Coordinates coordinates) throws Exception {
         if (this.userType == UserType.COMP) {
-            // считаем лучшие координаты и записываем в coordinates
+            // todo: считаем лучшие координаты и записываем в coordinates
+            coordinates = new Coordinates(0, 0);
         }
         if (coordinates == null) throw new Exception("Что-то пошло не так... Координаты не были переданы.");
-        this.move(board, coordinates);
+        GameManager.getCurrentGame().getBoard().claim(this, coordinates);
     }
 }
